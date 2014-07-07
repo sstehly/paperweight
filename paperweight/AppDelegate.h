51639d79892754c564c86ded076edacc08de65bb
sstehly <sstehly@sstehly-macbookpro2.roam.corp.google.com>
2014-07-06 21:12:09 -0700

    Initial Commit

---
+++ paperweight/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  paperweight
+//
+//  Created by Shaun Stehly on 7/6/14.
+//  Copyright (c) 2014 youtube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
